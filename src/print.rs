c8b4a2d08c4675943a09aa818ced37c3e043585a
Bhavya <bhavyasaini95@gmail.com>
2019-02-20 23:56:48 +0530

    println command basics

--- src/print.rs
+++ src/print.rs
@@ -1,8 +1,30 @@
-pub fn run(){
+pub fn run() {
     //printing to console
     println!("Hello from print.rs");
     //Basic Formatting
-    println!("Number: {}",1);
-    
-    println!("{} says hello to {}","Batman","Superman");
+    println!("Number: {}", 1);
+
+    println!("{} says hello to {}", "Batman", "Superman");
+
+    //Positional argument
+    println!(
+        "{0} and {1} fought with each other and {0} won",
+        "Batman", "Superman"
+    );
+
+    //Namd Argument
+    println!(
+        "My name is {name} and I am {dash} man alive",
+        name = "Barry Allen",
+        dash = "fastest"
+    );
+
+    //Placeholder trait
+    println!("Binary : {:b} Hexa : {:x} Octa : {:o}", 20, 7892, 88);
+
+    //Placeholde for debug traits
+    println!("{:?}", (12, true, 89));
+
+    //Basic math
+    println!("10 * 10 = {}", 10 * 10);
 }
