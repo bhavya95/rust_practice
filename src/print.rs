179c932aac76091c6610ece82ccd7437210ef350
Bhavya <bhavyasaini95@gmail.com>
2019-02-20 23:38:31 +0530

    first commit

---
+++ src/print.rs
@@ -0,0 +1,8 @@
+pub fn run(){
+    //printing to console
+    println!("Hello from print.rs");
+    //Basic Formatting
+    println!("Number: {}",1);
+    
+    println!("{} says hello to {}","Batman","Superman");
+}
